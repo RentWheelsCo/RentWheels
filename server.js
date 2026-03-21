955a221b4edf84f8af7b847f2b9a0d50f2622c7f
Jinisha Shrestha <jinishashr7@gmail.com>
2026-03-21 22:32:55 +0545

    feat: Create a server

---
+++ server.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import prisma from "./utils/db.js";
+import mainRouter from "./routes/index.routes.js";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", mainRouter);
+app.get("/", (req, res) => res.send(`Server is running on Port ${PORT}`));
+
+async function startServer() {
+    try {
+        await prisma.$connect();
+        console.log("Database connected successfully!");
+        app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    }
+}
+startServer();
