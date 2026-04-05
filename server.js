a91f06b3f57b3126ff85d9be618674c27862ea9f
Jinisha Shrestha <jinishashr7@gmail.com>
2026-04-05 11:15:27 +0545

    fix: CORS Update

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.APP_URL || 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use("/api", mainRouter);
 app.get("/", (req, res) => res.send(`Server is running on Port ${PORT}`));
